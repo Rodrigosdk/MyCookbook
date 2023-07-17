b8b3d4affcc8ed60ce752cf27a1b50cbf3fa4efe
Rodrigosdk <rodrigomamede53@gmail.com>
2023-07-16 22:47:40 -0300

    feat: :sparkles: Criado arquivo com as colunas basicas da tabela
    colunas que devem conter em quase todas as tabelas do banco de dados

---
+++ src/Backend/MyCookbook.Infrastructure/Migrations/BaseMigration.cs
@@ -0,0 +1,12 @@
+﻿using FluentMigrator.Builders.Create.Table;
+
+namespace MyCookbook.Infrastructure.Migrations;
+
+public static class BaseMigration
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax InsertColumnsDefalut(ICreateTableWithColumnOrSchemaOrDescriptionSyntax table)
+    {
+       return table.WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("DateTimeCreate").AsDateTime().NotNullable();
+    }
+}
