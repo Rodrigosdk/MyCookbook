43f0bd1917b3159e1fafd0fe0b54de984ef0e4de
Rodrigosdk <rodrigomamede53@gmail.com>
2024-01-22 00:51:22 -0300

    ✨ feat: mapped structure of the user table in the database.

---
+++ src/Backend/MyCookbook.Infrastructure/Database/Mappings/UserMap.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyCookbook.Domain.Entities;
+
+namespace MyCookbook.Infrastructure.Database.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("users");
+            
+            builder.HasKey(t => t.Id);
+            
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+            
+            builder.Property(t => t.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(t => t.Email)
+                .IsRequired()
+                .HasColumnName("Email")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(t => t.Password)
+                .IsRequired()
+                .HasColumnName("Password")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(2000);
+
+            builder.Property(t=> t.Phone)
+                .IsRequired()
+                .HasColumnName("Phone")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(14);
+
+            builder.Property(t=> t.CreatedAt)
+                .HasColumnName("DateTimeCreate")
+                .HasColumnType("SMALLDATETIME")
+                .HasMaxLength(60)
+                .HasDefaultValueSql("GETDATE()");
+
+        }
+    }
+}
