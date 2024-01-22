a939992cc7037e67368ddc566d142ba63671b752
Rodrigosdk <rodrigomamede53@gmail.com>
2024-01-22 00:46:25 -0300

    ✨ feat: create entity user

---
+++ src/Backend/MyCookbook.Domain/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿using MyCookbook.Domain.SeedWork;
+
+namespace MyCookbook.Domain.Entities
+{
+    public class UserEntity : Entity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; } 
+    }
+}
