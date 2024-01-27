3731df3481b34582aa4d97d8ff90e04e16677615
Rodrigosdk <rodrigomamede53@gmail.com>
2024-01-26 23:11:37 -0300

    ✨ feat: criado a classe responsável por realizar a injeção de dependências

---
+++ src/Backend/MyCookbook.Infrastructure/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyCookbook.Domain.Repositories;
+using MyCookbook.Infrastructure.Database;
+using MyCookbook.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyCookbook.Infrastructure;
+
+public static class Bootstrapper
+{
+    public static void AddRepository(this IServiceCollection services, IConfiguration configurationManager)
+    {
+        AddContext(services, configurationManager);
+        AddUnitOfWork(services);
+        AddRepositories(services);
+    }
+
+    public static void AddContext(IServiceCollection services, IConfiguration configurationManager)
+    {
+        var getConnectionString = configurationManager.GetConnectionString("Connection");
+        services.AddDbContext<DataContext>(optionsContext => optionsContext.UseSqlServer(getConnectionString));
+    }
+
+    public static void AddUnitOfWork(IServiceCollection services)
+    {
+        services.AddScoped<IUnitWorkRepository, UnitOfWork>();
+    }
+
+    public static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUsersRepository, UserRepository>();
+    }
+}
