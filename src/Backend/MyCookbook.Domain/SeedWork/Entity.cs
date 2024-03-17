a5ad34d5073288b254def6af861bebaa7401eb95
Rodrigosdk <rodrigomamede53@gmail.com>
2024-03-17 15:17:07 -0300

    ✨ feat: added altomatic hour value

--- src/Backend/MyCookbook.Domain/SeedWork/Entity.cs
+++ src/Backend/MyCookbook.Domain/SeedWork/Entity.cs
@@ -3,6 +3,6 @@
     public abstract class Entity
     {
         public int Id { get; protected set; }
-        public DateTime CreatedAt { get; protected set; }
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
     }
 }
