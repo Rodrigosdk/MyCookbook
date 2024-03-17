f524a644aede4627fcd3bcf7ce2083c8fe1a8e0d
Rodrigosdk <rodrigomamede53@gmail.com>
2024-03-17 14:50:42 -0300

    ✨ feat: add files for exceptions

---
+++ src/Shared/MyCookbook.Exceptions/ExceptionBase/MyCookbookException.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyCookbook.Exceptions.ExceptionBase;
+
+public class MyCookbookException : SystemException
+{
+}
---
+++ src/Shared/MyCookbook.Exceptions/ExceptionBase/ValidationErrorsException.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCookbook.Exceptions.ExceptionBase;
+
+public class ValidationErrorsException : MyCookbookException 
+{
+    public List<string> MessagesErrors { get; set; }
+
+    public ValidationErrorsException(List<string> messagesErrors)
+    {
+        MessagesErrors = messagesErrors;
+    }
+}
